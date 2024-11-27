56f3eff561935d848a6cbba8c386aa68dbc8592f
Kelsey Roche <roche.kelsey.24@gmail.com>
2024-11-27 12:41:45 -0500

    complete lab - core deliverables

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([])
+  
+  const [searchWord, setSearchWord] = useState("")
+
+  useEffect(() => {
+    fetch("http://localhost:6001/plants")
+    .then((response) => response.json())
+    .then((plantData) => setPlants(plantData))
+  }, [])
+
+  const handleSearch = (word) => {
+    setSearchWord(word)
+  }
+
+  const displayedPlants = plants.filter((plant) =>
+    plant.name.toLowerCase().includes(searchWord.toLowerCase())
+  )
+  
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm setPlants={setPlants}/>
+      <Search onSearch={handleSearch}/>
+      <PlantList plants={displayedPlants} setPlants = {setPlants}/>
     </main>
   );
 }
